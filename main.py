3c16862c822807ea56a025a8a523f4a2af52471b
LaryScript <dfghhgfd2002@gmail.com>
2023-08-07 12:36:52 +0500

    first commit1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot= telebot.TeleBot('6207441183:AAHtInN2yb7ZbF3IWAeY2pZyrOvTz9Bpzz4')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, 'Привет тест!')
 
 bot.infinity_polling()
 
